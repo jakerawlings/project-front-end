b817436630b88b8ae79110b944fa9383f7fcdf70
jakerawlings37 <jakerawlings37@gmail.com>
2021-12-11 10:13:31 -0500

    i cant believe this

--- src/App.js
+++ src/App.js
@@ -19,27 +19,4 @@ function App() {
   );
 }
 
-export default App;import Login from "./login/Login";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./Home";
-import Register from "./login/Register";
-import Profile from "./login/Profile";
-
-function App() {
-  return (
-      <div className="container">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-          </Routes>
-        </BrowserRouter>
-      </div>
-  );
-}
-
-export default App;
-
 export default App;
