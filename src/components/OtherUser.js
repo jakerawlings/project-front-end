bcae65b336efd67defde8d4f2ac51387196fd009
jakerawlings37 <jakerawlings37@gmail.com>
2021-12-14 23:13:38 -0500

    added others profiles

--- src/components/OtherUser.js
+++ src/components/OtherUser.js
@@ -1,22 +1,12 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import Navigation from "./Navigation";
+import {Link, useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
 import {API_URL} from "../consts";
+import Navigation from "./Navigation";
 
 const OtherUser = () => {
   const params = useParams();
 
-  // const findUserbyID = () =>
-  //     fetch(`${API_URL}/users/${params.id}`, {
-  //       method: 'GET',
-  //       credentials: 'include',
-  //       mode: "cors"
-  //     })
-  //     .then(res => res.json())
-  //     .then(user => setUserDetails(user));
-  //
-  // const [userDetails, setUserDetails] = useState({favorites: ''});
-  // useEffect(findUserbyID, []);
+  let userDetails = {id: 0, favorites: []}
 
   const getAllUsers = () =>
       fetch(`${API_URL}/get-users`, {
@@ -26,22 +16,35 @@ const OtherUser = () => {
       }).then(res => res.json())
       .then(users => setUsers(users));
 
-  const getUser = (userId) => {
-    users.map(user => {
-      if (user._id === userId) {
-        setUser(user)
-      }
-    })
-  }
-
   const [users, setUsers] = useState([])
-  const [user, setUser] = useState({favorites: []})
   useEffect(getAllUsers, []);
-  useEffect(getUser(params.id), []);
 
   return (
       <div>
-        {JSON.stringify(users)}
+        {users.map(user => {
+          console.log(user._id === params.id ?
+              userDetails = user : console.log("lol"))
+        })}
+        <h1 className="display-1 text-center text-danger">{userDetails.username}'s
+          Profile</h1>
+        <Navigation active=""/>
+        <h3 className="text-left mt-3 text-success">{userDetails.username}'s
+          Favorites</h3>
+        <ul className="list-group">
+          {userDetails.favorites.map(movie =>
+              <li className="list-group-item">
+                <Link to={`/details/${movie.imdbID}`}>
+                  <h4>{movie.Title}</h4>
+                  <img src={movie.Poster} height="300px"/>
+                </Link>
+              </li>
+          )}
+        </ul>
+        {
+          userDetails.role === "Critic" && <h3
+              className="text-left mt-3 text-success">{userDetails.username}'s
+            Reviews</h3>
+        }
       </div>
   );
 };
